2f7901dcc5a53f87b1a105aa77bd67418e225915
kunljha <jha711kunal@gmail.com>
2021-08-09 19:55:16 +0530

    fixed some minor changes

--- js/todos.js
+++ js/todos.js
@@ -1,43 +1,43 @@
-'use strict';
+'use strict'
 
 // DOM Manipulation
-let todos = readingTodos();
+let todos = readingTodos()
 
 // filtering
 const filters = {
 	searchText: '',
 	isChecked: false,
-};
+}
 
-renderTodos(todos, filters); // to show initial todos to user
+renderTodos(todos, filters) // to show initial todos to user
 
-// All Event-Listerner in ToDo-App
+// All Event-Listener in Todo-App
 
 // event-listener for filtering todos
 document.querySelector('#filter-todo').addEventListener('input', (e) => {
-	filters.searchText = e.target.value;
-	renderTodos(todos, filters);
-});
+	filters.searchText = e.target.value
+	renderTodos(todos, filters)
+})
 
 // event-listener for adding todos
 document.querySelector('#add-todo').addEventListener('submit', (e) => {
-	e.preventDefault(); // prevents default actions of submit, like page reload
-	const todoText = e.target.elements.addTodo.value.trim();
+	e.preventDefault() // prevents default actions of submit, like page reload
+	const todoText = e.target.elements.addTodo.value.trim()
 	if (todoText.length > 0) {
 		todos.push({
 			id: uuidv4(),
 			task: todoText,
 			completed: false,
-		});
+		})
 
-		savedTodos(todos); // adding new todos in localStorage
-		renderTodos(todos, filters);
-		e.target.elements.addTodo.value = '';
+		savedTodos(todos) // adding new todos in localStorage
+		renderTodos(todos, filters)
+		e.target.elements.addTodo.value = ''
 	}
-});
+})
 
 // event-listener to show incomplete todos
 document.querySelector('#tasks-left').addEventListener('change', (e) => {
-	filters.isChecked = e.target.checked;
-	renderTodos(todos, filters);
-});
+	filters.isChecked = e.target.checked
+	renderTodos(todos, filters)
+})
